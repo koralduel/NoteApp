e2f6e0775fc3182ecd007079a4d62746d6949f42
koralduel <koralduel3112@gmail.com>
2023-01-28 19:05:30 +0200

    get location and save it to db

--- app/src/main/java/com/example/noteapp/CreateNote.java
+++ app/src/main/java/com/example/noteapp/CreateNote.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -35,6 +39,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 
@@ -45,7 +50,6 @@ public class CreateNote extends AppCompatActivity implements LocationListener {
     FirebaseAuth mAuth;
     NotesViewModel viewModel = new NotesViewModel();
     LocationManager locationManager;
-    LocationListener locationListener;
     String txtLocation;
     Note note;
 
@@ -68,24 +72,15 @@ public class CreateNote extends AppCompatActivity implements LocationListener {
 
             binding.TvCreationDateValue.setText(creationDate);
 
-           /* locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
+            if(ContextCompat.checkSelfPermission(CreateNote.this,Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(CreateNote.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},100);
             }
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);*/
-
             binding.BtnSave.setOnClickListener(view->{
                 String title = binding.ETTitleValue.getText().toString();
                 String body = binding.ETBodyValue.getText().toString();
                 String uid = UUID.randomUUID().toString();
+                getLocation();
                 String location = txtLocation;
                 Note note = new Note(creationDate,title,body,uid,location);
 
@@ -133,9 +128,31 @@ public class CreateNote extends AppCompatActivity implements LocationListener {
 
     }
 
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        try{
+            locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,5,CreateNote.this);
+            Geocoder geocoder = new Geocoder(CreateNote.this, Locale.getDefault());
+            Location currentLocation =  locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            List<Address> addresses = geocoder.getFromLocation(currentLocation.getLatitude(),currentLocation.getLongitude(),1);
+            txtLocation = addresses.get(0).getAddressLine(0);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onLocationChanged(@NonNull Location location) {
-         txtLocation = "latitude"+ location.getLatitude() + ","+"longitude" + location.getLongitude();
+        try{
+            Geocoder geocoder = new Geocoder(CreateNote.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+            txtLocation = addresses.get(0).getAddressLine(0);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
